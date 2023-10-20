58890b03374e216b40c91142b8c708e0b599652f
vladtarnovskiy <vladtarnovski989@gmail.com>
2023-10-20 03:36:40 +0300

    feat: add metadata

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,5 +1,11 @@
 "use client";
 import { SignInForm } from "@/components/SignInForm";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Sign In",
+  description: "Sign in page",
+};
 
 export default function Signin() {
   return <SignInForm />;
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,5 +1,11 @@
 "use client";
 import { RegistrationForm } from "@/components/RegistrationForm";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Sign Up",
+  description: "Sign up page",
+};
 
 export default function Signup() {
   return <RegistrationForm />;
