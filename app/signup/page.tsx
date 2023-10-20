54c471d54316ebd6594bd02f939effac86d498e5
vladtarnovskiy <vladtarnovski989@gmail.com>
2023-10-20 03:37:59 +0300

    fix: remove metadata

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,11 +1,5 @@
 "use client";
 import { RegistrationForm } from "@/components/RegistrationForm";
-import { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "Sign Up",
-  description: "Sign up page",
-};
 
 export default function Signup() {
   return <RegistrationForm />;
